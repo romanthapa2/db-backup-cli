20e0384c62cf07a4c54077f8e374f9030dd1dad4
romanthapa2 <thaparoman970@gmail.com>
2025-03-28 16:42:47 +0545

    database connectors

--- mongodb/mongodb-backup.js
+++ mongodb/mongodb-backup.js
@@ -1,25 +1,32 @@
 import chalk from "chalk";
- import { exec } from "child_process";
- 
- export async function createMongoDBBackup(config) {
- try {
-     let command = `mongodump --uri=${config.uri} --db=${config.databaseName} --out=${config.outputDir}`;
-     
-     console.log(chalk.blue("Creating backup..."));
-     console.log(chalk.blue("Executing command: ", command));
- 
-     exec(command, (error, _, stderr) => {
-       if (error || !stderr) {
-         console.log(
-           chalk.red("Error creating backup, Please check the Config values")
-         );
-         return;
-       }
-       console.log(
-         chalk.green("Backup created successfully at ", config.outputDir)
-       );
-     });
-   } catch (error) {
-     console.log(chalk.red("Error creating backup"));
-   }
- }
+import { exec } from "child_process";
+import { testMongoDBConnection } from "../databaseConnectors.js";
+import ora from "ora";
+
+export async function createMongoDBBackup(config) {
+  try {
+    let command = `mongodump --uri=${config.uri} --db=${config.databaseName} --out=${config.outputDir}`;
+    await testMongoDBConnection(config);
+
+    console.log(chalk.blue("Creating backup..."));
+    console.log(chalk.blue("Executing command: ", command));
+
+    exec(command, (error, _, stderr) => {
+      if (error || !stderr) {
+        console.log(
+          spinner.fail(
+            chalk.red("Error creating backup, Please check the Config values")
+          )
+        );
+        return;
+      }
+      console.log(
+        spinner.succeed(
+          chalk.green("Backup created successfully at ", config.outputDir)
+        )
+      );
+    });
+  } catch (error) {
+    spinner.fail(chalk.red("Error creating backup"));
+  }
+}
